b4d75b34e31d148ffedb36e850130db380d6b217
Timchenko Daniil <daniil.timchenko@nfp2b.com>
2022-12-07 14:05:17 +0300

    Warnings - при пустом SELECT в запросе

--- archivers/common_data_archiver.py
+++ archivers/common_data_archiver.py
@@ -76,13 +76,12 @@ class CommonDataArchiver:
 
         if equals_to is None:
             data = self.conn.copy_table(from_schema_table, to_schema_table, from_columns)
-            self.logger.info(f"Таблица '{from_schema_table}' скопирована в таблицу '{to_schema_table}'")
         else:
             data = self.conn.copy_table_where(from_schema_table, to_schema_table,
                                               from_columns, where_col, equals_to)
-            self.logger.info(f"Таблица '{from_schema_table}'  скопирована в таблицу '{to_schema_table}''"
-                  f" ГДЕ '{where_col}' == {equals_to}")
-        self._report_empty_result(data, from_schema_table)
+            specification = f"ГДЕ '{where_col}' == {equals_to}"
+
+        self._report_results(data, from_schema_table, specification, to_schema_table)
 
     def copy_tables(self, from_tables, to_tables, where_cols, equal_to_values):
         """
@@ -109,6 +108,8 @@ class CommonDataArchiver:
 
         data = self.conn.delete_table_where(schema_table_name, where_col, equal_to)
         self.logger.info(f"Ряды таблицы '{schema_table_name} ГДЕ '{where_col}' == {equal_to} УДАЛЕНЫ")
+        if self._query_result_empty(data):
+            self._report_results(data, schema_table_name)
         self._report_empty_result(data, schema_table_name)
 
     def delete_tables(self, tables: list[str], where_cols: list[str], equal_to_values: list[object]):
@@ -142,8 +143,20 @@ class CommonDataArchiver:
         meta_dataset_col = upload_files_table["req_cols"]["metaload_dataset_id"]
         self.delete_table(table=table, where_col=meta_dataset_col, equal_to=self.meta_dataset_id)
 
-    def add_to_logger(self, msg: str):
-        self.logger += msg
+    def _report_results(self, data, from_schema_table, to_schema_table, where_col, equal_to, specification, mode=None):
+        match mode:
+            case "copy":
+                if self._query_result_empty(data):
+                    self.logger.warning(f"Из таблицы '{from_schema_table}' не было найдено ни одного ряда по данному фильтру!")
+                else:
+                    self.logger.info(f"Таблица '{from_schema_table}' скопирована в таблицу '{to_schema_table}' {specification}")
+            case "delete":
+                if self._query_result_empty(data):
+                    self.logger.warning(f"Из таблицы '{from_schema_table}' не было найдено ни одного ряда по данному фильтру!")
+                else:
+                    self.logger.info(f"Ряды таблицы '{from_schema_table} ГДЕ '{where_col}' == {equal_to} УДАЛЕНЫ")
+            case _:
+                raise NotImplementedError()
 
     def _get_intersecting_columns(self, from_schema, from_table, to_schema, to_table):
         from_columns = self.conn.get_column_names(self._sql_name(from_schema, from_table))
@@ -236,10 +249,10 @@ class CommonDataArchiver:
         return AsIs(schema_and_table)
 
     def _report_empty_result(self, data, schema_table_name):
-        return
         if self._query_result_empty(data):
-            self.logger.info(f"\nWARNING! Query result from Table '{schema_table_name}' was empty!\n")
-
+            self.logger.warning(f"Из таблицы '{schema_table_name}' не было найдено ни одного ряда по данному фильтру!")
+            return True
+        return False
     def _query_result_empty(self, data):
         empty_list = len(data) == 0
         none_type = not data
